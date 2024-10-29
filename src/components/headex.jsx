22e31854d1c7f8229a4ebdd38fc61907ae25485c
lucadev22 <luca_dev22@outlook.com>
2024-10-29 15:24:18 -0300

    test header

--- src/components/headex.jsx
+++ src/components/headex.jsx
@@ -2,16 +2,19 @@ import Logo from "./../assets/3pounds_logo.png"
 
 const Header = () => {
     return ( 
-        <>
-        <img src={Logo} width={128} height={"auto"}></img>
+        <div style={{display:"flex", justifyContent:"center", height:"50px"}}>
+        <img src={Logo} width={"auto"} height={"44px"} ></img>
+        
         <h1 style={{fontFamily:"Figtree", 
         color:"#df6c7c",
         fontSize: "2.3em",
          marginTop: "0.2em",
          paddingBottom: "0.3em",
-          borderBottom: "1px solid #dbcdcf"}}>3pounds
+         marginLeft: "0.2em",
+         }}>3pounds
         <span style={{color:"#dbcdcf"}}>.fit</span></h1>
-        </>
+        
+        </div>
      );
 }
  
