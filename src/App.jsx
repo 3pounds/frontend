2ad7aa9eee2fd3bb878afe6e195add42711fee00
lucadev22 <luca_dev22@outlook.com>
2024-10-31 19:37:03 -0300

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Header from "./components/headex.jsx"
 const App = () => {
   
   const [questions, setQuestions] = useState([])
-  const {handleChangeForm, globalForm} = useContext(Context)
+  const {handleChangeForm, globalForm, handleSubmit} = useContext(Context)
   // const [startingQuestion, setStartingQuestion] = useState("162")
   // const [currentQuestion, setCurrentQuestion] = useState("2")
   const [currentOutlineNumber, setCurrentOutlineNumber] = useState("1")
@@ -21,22 +21,28 @@ const App = () => {
   const [pendingQuestions, setPendingQuestions] = useState([])
 
   const [endQuestion, setEndQuestion] = useState(false)
-
+  
 
   const onClickContinue = () => { 
     console.log("onClick")     
+    console.log("currentOptions", currentOptions.length)
+    if(currentOptions.length > 0) {
+      currentOptions.forEach((node) => {
+        if(globalForm[node.ID] == true){
+          // setCurrentQuestion(node.ID)
+          setCurrentOutlineNumber(node.OutlineNumber)
+          setCurrentOutlineLevel((parseInt(node.OutlineLevel)+1).toString()) // 2 levels
+        }
+      })
+    }
+    else {
+      handlePendingQuestions()
+    }
    
-    currentOptions.forEach((node) => {
-      if(globalForm[node.ID] == true){
-        // setCurrentQuestion(node.ID)
-        setCurrentOutlineNumber(node.OutlineNumber)
-        setCurrentOutlineLevel((parseInt(node.OutlineLevel)+1).toString()) // 2 levels
-      }
-    })
-   
+  
     setCurrentOptions([]) //reset options
     setCurrentQuestions([]) //reset questions
-    
+
   }
 
   const findByOutlineNumber = (data, OutlineNumber) => {
@@ -57,13 +63,33 @@ const App = () => {
     return null; // Return null if no match is found
   }
 
-  const checkEnd = () => {
+  const findByOutlineNumber2 = (data, id) => {
+    for (const item of data) {
+      if (item.OutlineNumber === id) {  // If the current item matches the id, return it
+        return item;
+      }
+      
+      // If the current item has nested children, search within them
+      if (item.children && item.children.length > 0) {
+        const result = findByOutlineNumber2(item.children, id);
+        if (result) {
+          return result;
+        }
+      }
+    }
     
-    let child = currentOutlineNumber  + ".1"
-    let find = findByOutlineNumber(data, child)
+    // If no match is found, return null
+    return null;
+  }
+
+  const checkNoMoreChilds = (currentOutlineNumber) => {
     
+    let child = currentOutlineNumber + ".1"
+    let find = findByOutlineNumber2(data, child)
+    // console.log("checkiedFor", child, "result:", find)
     if(!find) {
       if(pendingQuestions.length > 0) {
+       
         handlePendingQuestions()
       }
       else {
@@ -80,9 +106,10 @@ const App = () => {
         setCurrentOutlineNumber(nextQuestion.OutlineNumber)
         setCurrentOutlineLevel(nextQuestion.OutlineLevel) 
         setCurrentQuestions([pendingQuestions[0]])
-        let arr = pendingQuestions.shift()
+        const arr = pendingQuestions.slice(1);        
+        console.log("pendingQuestionsarr",arr)
         setPendingQuestions(arr)
-        console.log("pendingQuestions", pendingQuestions)
+      
   }
 
   const checkLevel = (stringLevel) => { //checks if the level matches with current outlinelevel or the one under (options)
@@ -102,7 +129,7 @@ const App = () => {
   }
 
   const handleCurrentQuestions = (node) => {
-    console.log("handleCurrentQuestions", node)
+    // console.log("handleCurrentQuestions", node)
     let arr = []
     arr = currentQuestions
     console.log("arr", arr)
@@ -152,7 +179,7 @@ const App = () => {
   useEffect(() => {
     if(endQuestion) {
       console.log("ended")
-      console.log(JSON.stringify(globalForm, null, 2))
+      handleSubmit()
     }
   }, [endQuestion]);
 
@@ -165,21 +192,25 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log("pendingquestionsarr CurrentQuesitons", currentQuestions.length)
-    if(currentQuestions.length > 1){
+    console.log("CurrentQuesitons", currentQuestions.length)
+    // && pendingQuestions.length == 0
+    if(currentQuestions.length > 1 ){
       let arr = []
       for(let i = 0; i < currentQuestions.length; i++) {
         if(i > 0){
           arr.push(currentQuestions[i])
+          console.log("pendingquestionsarr", currentQuestions[i].Name)
         }
       }
       console.log("pendingquestionsarr", arr)
-      setPendingQuestions(arr)
+      let finalArr = [...arr, ...pendingQuestions]
+      console.log("pendingquestionsarr", finalArr)
+      setPendingQuestions(finalArr)
     }
   }, [currentQuestions])
 
   useEffect(() => {
-    checkEnd()
+    checkNoMoreChilds(currentOutlineNumber)
   }, [currentOutlineNumber]);
   
   
@@ -191,24 +222,33 @@ const App = () => {
       {(checkStart(node.OutlineNumber) && checkLevel(node.OutlineLevel)) &&
        
       <>
-      {/* {console.log(node.OutlineLevel, node.OutlineNumber, node.Name, currentOutlineNumber)} */}
-     
+        {/* {console.log(node.OutlineLevel, node.OutlineNumber, node.Name, currentOutlineNumber)} */}
         {/* {node.children.length > 0 ?  */}
         {/* {console.log(node.OutlineLevel, currentOutlineLevel)} */}
         
         {node.OutlineLevel == currentOutlineLevel ? 
          <>
-         {handleCurrentQuestions(node)}
-         {/* {console.log("currentQuestions", currentQuestions)} */}
-         {node.OutlineNumber == currentQuestions[0].OutlineNumber ?
-          /* {node.OutlineLevel}  */
-          <div className="questQuestion">
-          <h2>{node.Name}</h2>
-          </div>
-          :
-          <>
-          </>
-         }
+          {handleCurrentQuestions(node)}
+          {/* {console.log("currentQuestions", currentQuestions)} */}
+          {node.OutlineNumber == currentQuestions[0].OutlineNumber ?
+            /* {node.OutlineLevel}  */
+            <>
+            {node.children.length == 0 ?
+            <>
+             <h2>Solution:</h2>
+             <h3>{node.Name}</h3> 
+            </>
+            :
+            <div className="questQuestion">
+              <h2>{node.Name}</h2>
+            </div>
+            }
+            </>
+
+            :
+            <>
+            </>
+          }
          </>
           : 
           node.OutlineNumber.startsWith(currentQuestions[0].OutlineNumber) ?
@@ -222,7 +262,7 @@ const App = () => {
             name={node.ID} 
             checked={globalForm[node.ID]}
           />
-          <label style={{fontSize: "18px", marginLeft:"6px"}} for={node.ID}>{node.Name}</label>
+          <label style={{fontSize: "18px", marginLeft:"6px"}}>{node.Name}</label>
           </div>
           :
           <>
@@ -233,9 +273,8 @@ const App = () => {
       }
 
         {/* <p><strong>Outline Number:</strong> {node.OutlineNumber}
-        <strong>Outline Level:</strong> {node.OutlineLevel}</p> */}
-        {/* <ul></ul> */}
-  
+        <strong>Outline Level:</strong> {node.OutlineLevel}</p> */}    
+
         {/* If there are children, recursively render them */}
         {node.children && node.children.length > 0 && (
           <div>
@@ -258,12 +297,12 @@ const App = () => {
         {questions.length > 0 && 
             <>
             {questions.map((node) => (
-              console.log("node", node),
+              /* console.log("node", node), */
               <RecursiveNode key={node.ID} node={node} />
             ))}
-            </>
+            <button style={{marginTop: "14px"}} onClick={() => onClickContinue()}> Continue </button>
+          </>
         }
-        <button style={{marginTop: "14px"}} onClick={() => onClickContinue()}> Continue </button>
       </div>
      :
       <>
